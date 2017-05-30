a556565082bc6190a6bc027ed833e8288a34f02f
Peter Inglesby <peter.inglesby@gmail.com>
2017-05-30 17:54:25 +0100

    Add AUTH_USER_MODEL setting

--- ironcage/settings.py
+++ ironcage/settings.py
@@ -115,6 +115,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Custom User model
+
+AUTH_USER_MODEL = 'accounts.User'
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
