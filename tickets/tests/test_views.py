8f69f084bf233f0e7d89887392f5dbf632cd66f6
Peter Inglesby <peter.inglesby@gmail.com>
2017-05-31 07:27:32 +0100

    Shorten names of some view tests

--- tickets/tests/test_views.py
+++ tickets/tests/test_views.py
@@ -13,24 +13,14 @@ class NewOrderTests(TestCase):
     def setUpTestData(cls):
         cls.alice = User.objects.create_user(email_addr='alice@example.com', name='Alice')
 
-    def test_get_new_order_when_not_authenticated(self):
-        rsp = self.client.get('/tickets/orders/new/')
-        self.assertInHTML('<tr><td>5 days</td><td>£138</td><td>£276</td></tr>', rsp.content.decode())
-        self.assertNotContains(rsp, '<form method="post" id="order-form">')
-        self.assertContains(rsp, 'Please create an account to buy a ticket.')
-
-    def test_post_new_order_when_not_authenticated(self):
-        rsp = self.client.post('/tickets/orders/new/', follow=True)
-        self.assertRedirects(rsp, '/accounts/login/')
-
-    def test_get_new_order(self):
+    def test_get(self):
         self.client.force_login(self.alice)
         rsp = self.client.get('/tickets/orders/new/')
         self.assertInHTML('<tr><td>5 days</td><td>£138</td><td>£276</td></tr>', rsp.content.decode())
         self.assertContains(rsp, '<form method="post" id="order-form">')
         self.assertNotContains(rsp, 'Please create an account to buy a ticket.')
 
-    def test_post_new_order_for_self(self):
+    def test_post_for_self(self):
         self.client.force_login(self.alice)
         form_data = {
             'who': 'self',
@@ -47,7 +37,7 @@ class NewOrderTests(TestCase):
         rsp = self.client.post('/tickets/orders/new/', form_data, follow=True)
         self.assertContains(rsp, 'You have ordered 1 ticket(s)')
 
-    def test_post_new_order_for_others(self):
+    def test_post_for_others(self):
         self.client.force_login(self.alice)
         form_data = {
             'who': 'others',
@@ -64,7 +54,7 @@ class NewOrderTests(TestCase):
         rsp = self.client.post('/tickets/orders/new/', form_data, follow=True)
         self.assertContains(rsp, 'You have ordered 2 ticket(s)')
 
-    def test_post_new_order_for_self_and_others(self):
+    def test_post_for_self_and_others(self):
         self.client.force_login(self.alice)
         form_data = {
             'who': 'self and others',
@@ -81,6 +71,16 @@ class NewOrderTests(TestCase):
         }
         rsp = self.client.post('/tickets/orders/new/', form_data, follow=True)
         self.assertContains(rsp, 'You have ordered 3 ticket(s)')
+
+    def test_get_when_not_authenticated(self):
+        rsp = self.client.get('/tickets/orders/new/')
+        self.assertInHTML('<tr><td>5 days</td><td>£138</td><td>£276</td></tr>', rsp.content.decode())
+        self.assertNotContains(rsp, '<form method="post" id="order-form">')
+        self.assertContains(rsp, 'Please create an account to buy a ticket.')
+
+    def test_post_when_not_authenticated(self):
+        rsp = self.client.post('/tickets/orders/new/', follow=True)
+        self.assertRedirects(rsp, '/accounts/login/')
 
 
 class OrderTests(TestCase):
@@ -102,12 +102,12 @@ class OrderTests(TestCase):
         rsp = self.client.get(f'/tickets/orders/{self.order.order_id}/', follow=True)
         self.assertContains(rsp, f'Details of your order ({self.order.order_id})')
 
-    def test_order_when_not_authenticated(self):
+    def test_when_not_authenticated(self):
         rsp = self.client.get(f'/tickets/orders/{self.order.order_id}/', follow=True)
         self.assertRedirects(rsp, f'/accounts/login/?next=/tickets/orders/{self.order.order_id}/')
         self.assertContains(rsp, 'Please login to see this page.')
 
-    def test_order_when_not_authorized(self):
+    def test_when_not_authorized(self):
         bob = User.objects.create_user(email_addr='bob@example.com')
         self.client.force_login(bob)
         rsp = self.client.get(f'/tickets/orders/{self.order.order_id}/', follow=True)
@@ -133,7 +133,7 @@ class OrderPaymentTests(TestCase):
         self.client.force_login(self.alice)
         self.order.refresh_from_db()
 
-    def test_order_payment_success(self):
+    def test_stripe_success(self):
         with patched_charge_creation_success():
             rsp = self.client.post(
                 f'/tickets/orders/{self.order.order_id}/payment/',
@@ -143,7 +143,7 @@ class OrderPaymentTests(TestCase):
         self.assertContains(rsp, 'Payment for this order has been received')
         self.assertNotContains(rsp, '<div id="stripe-form">')
 
-    def test_order_payment_failure(self):
+    def test_stripe_failure(self):
         with patched_charge_creation_failure():
             rsp = self.client.post(
                 f'/tickets/orders/{self.order.order_id}/payment/',
@@ -174,12 +174,12 @@ class TicketTests(TestCase):
         rsp = self.client.get(f'/tickets/tickets/{self.ticket.ticket_id}/', follow=True)
         self.assertContains(rsp, f'Details of your ticket ({self.ticket.ticket_id})')
 
-    def test_ticket_when_not_authenticated(self):
+    def test_when_not_authenticated(self):
         rsp = self.client.get(f'/tickets/tickets/{self.ticket.ticket_id}/', follow=True)
         self.assertRedirects(rsp, f'/accounts/login/?next=/tickets/tickets/{self.ticket.ticket_id}/')
         self.assertContains(rsp, 'Please login to see this page.')
 
-    def test_ticket_when_not_authorized(self):
+    def test_when_not_authorized(self):
         bob = User.objects.create_user(email_addr='bob@example.com')
         self.client.force_login(bob)
         rsp = self.client.get(f'/tickets/tickets/{self.ticket.ticket_id}/', follow=True)
@@ -202,19 +202,19 @@ class TicketInvitationTests(TestCase):
         cls.invitation = TicketInvitation.objects.get(email_addr='bob@example.com')
         cls.bob = User.objects.create_user(email_addr='bob@example.com', name='Bob')
 
-    def test_ticket_invitation_when_not_authenticated(self):
-        rsp = self.client.get(f'/tickets/invitations/{self.invitation.token}/', follow=True)
-        self.assertContains(rsp, '<div class="alert alert-info" role="alert">You need to create an account to claim your invitation</div>', html=True)
-
-    def test_ticket_invitation_for_unclaimed_invitation(self):
+    def test_for_unclaimed_invitation(self):
         self.client.force_login(self.bob)
         rsp = self.client.get(f'/tickets/invitations/{self.invitation.token}/', follow=True)
         self.assertContains(rsp, 'Details of your ticket (9A19)')
         self.assertNotContains(rsp, 'This invitation has already been claimed', html=True)
 
-    def test_ticket_invitation_for_claimed_invitation(self):
+    def test_for_claimed_invitation(self):
         self.client.force_login(self.bob)
         actions.claim_ticket_invitation(self.bob, self.invitation)
         rsp = self.client.get(f'/tickets/invitations/{self.invitation.token}/', follow=True)
         self.assertContains(rsp, 'Details of your ticket (9A19)')
         self.assertContains(rsp, '<div class="alert alert-info" role="alert">This invitation has already been claimed</div>', html=True)
+
+    def test_when_not_authenticated(self):
+        rsp = self.client.get(f'/tickets/invitations/{self.invitation.token}/', follow=True)
+        self.assertContains(rsp, '<div class="alert alert-info" role="alert">You need to create an account to claim your invitation</div>', html=True)
