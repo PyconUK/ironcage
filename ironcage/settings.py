e5175f6237eb243ca1757865c9e9426fc8dc30af
Peter Inglesby <peter.inglesby@gmail.com>
2017-06-13 07:11:47 +0100

    Add STATIC_ROOT

--- ironcage/settings.py
+++ ironcage/settings.py
@@ -145,6 +145,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Stripe
 
 STRIPE_API_KEY_PUBLISHABLE = os.environ.get('STRIPE_API_KEY_PUBLISHABLE', ENVVAR_SENTINAL)
