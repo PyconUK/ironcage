8da7945749c2d0ed063aea485ed94f48391aecda
Peter Inglesby <peter.inglesby@gmail.com>
2017-06-15 07:45:21 +0100

    Ensure we don't have too many new lines in emails

--- tickets/mailer.py
+++ tickets/mailer.py
@@ -3,6 +3,8 @@ from django.core.mail import send_mail
 from django.template.loader import get_template
 from django.urls import reverse
 
+import re
+
 
 INVITATION_TEMPLATE = '''
 Hello!
@@ -45,7 +47,8 @@ def send_order_confirmation_mail(order):
         'ticket_for_self': order.ticket_for_self(),
         'receipt_url': settings.DOMAIN + reverse('tickets:order_receipt', args=[order.order_id]),
     }
-    body = template.render(context)
+    body_raw = template.render(context)
+    body = re.sub('\n\n\n+', '\n\n', body_raw)
 
     send_mail(
         f'PyCon UK 2017 order confirmation ({order.order_id})',
