e149f23cee9d0d383ac83b2f3359f8b245392489
Peter Inglesby <peter.inglesby@gmail.com>
2017-06-15 07:09:05 +0100

    Add command to test email sending

---
+++ ironcage/management/commands/sendtestemail.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from django.core.mail import send_mail
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('to_addr')
+
+    def handle(self, *args, **kwargs):
+        send_mail(
+            'PyCon UK 2017 test email',
+            f'This is a test, generated at {datetime.now()}',
+            'PyCon UK 2017 <tickets@pyconuk.org>',  # TODO either set up this address, or find another one to use
+            [kwargs['to_addr']],
+            fail_silently=False,
+        )
