8a21114af37349f845b2a1815f1b0d6657f9d705
Peter Inglesby <peter.inglesby@gmail.com>
2017-06-15 07:37:57 +0100

    Fix tests that check for URL in emails

--- ironcage/settings.py
+++ ironcage/settings.py
@@ -38,7 +38,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['ironcage-dev.herokuapp.com']
 
 # A custom setting for creating full URLs in links in emails
-DOMAIN = ALLOWED_HOSTS[0]
+DOMAIN = f'https://{ALLOWED_HOSTS[0]}'
 
 # Application definition
 
