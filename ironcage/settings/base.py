260d7db68d7fb5b632907f0625b290b50fff3261
Peter Inglesby <peter.inglesby@gmail.com>
2017-06-17 11:52:13 +0100

    Start move to settings directory

--- ironcage/settings/base.py
+++ ironcage/settings/base.py
@@ -15,7 +15,8 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(BASE_DIR)
 
 # Used for distinguishing settings that are unset in production.
 ENVVAR_SENTINAL = 'not-for-production'
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import dotenv
 
 if __name__ == "__main__":
     dotenv.read_dotenv()
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ironcage.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ironcage.settings.base")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
