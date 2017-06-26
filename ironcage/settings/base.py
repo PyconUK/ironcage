02bf088df826de42dc612f425e576e61d772b015
Peter Inglesby <peter.inglesby@gmail.com>
2017-06-26 14:01:49 +0100

    Add some logging config to log exceptions to console

--- ironcage/settings/base.py
+++ ironcage/settings/base.py
@@ -148,6 +148,24 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
 # Stripe
 
 STRIPE_API_KEY_PUBLISHABLE = os.environ.get('STRIPE_API_KEY_PUBLISHABLE', ENVVAR_SENTINAL)
