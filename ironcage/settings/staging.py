83e73594780e016420c91680d1b8aeb1e8dc70ca
Peter Inglesby <peter.inglesby@gmail.com>
2017-06-28 11:21:10 +0100

    Require SSL on staging

--- ironcage/settings/staging.py
+++ ironcage/settings/staging.py
@@ -4,3 +4,7 @@ ALLOWED_HOSTS = ['hq-staging.pyconuk.org']
 
 # A custom setting for creating full URLs in links in emails
 DOMAIN = f'https://{ALLOWED_HOSTS[0]}'
+
+# SSL
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
