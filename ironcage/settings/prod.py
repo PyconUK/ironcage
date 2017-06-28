e796592fd9acfdfb72748d1c8f9d247186b0e28b
Peter Inglesby <peter.inglesby@gmail.com>
2017-06-28 12:09:53 +0100

    Add prod settings file

---
+++ ironcage/settings/prod.py
@@ -0,0 +1,10 @@
+from .base import *  # flake8: noqa
+
+ALLOWED_HOSTS = ['hq.pyconuk.org']
+
+# A custom setting for creating full URLs in links in emails
+DOMAIN = f'https://{ALLOWED_HOSTS[0]}'
+
+# SSL
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
