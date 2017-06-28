646bd5ff25df70eb27f2d80b87cc50b60b95b1f8
Peter Inglesby <peter.inglesby@gmail.com>
2017-06-28 11:16:54 +0100

    Update ALLOWED_HOSTS

--- ironcage/settings/staging.py
+++ ironcage/settings/staging.py
@@ -1,6 +1,6 @@
 from .base import *  # flake8: noqa
 
-ALLOWED_HOSTS = ['ironcage-dev.herokuapp.com']
+ALLOWED_HOSTS = ['hq-staging.pyconuk.org']
 
 # A custom setting for creating full URLs in links in emails
 DOMAIN = f'https://{ALLOWED_HOSTS[0]}'
