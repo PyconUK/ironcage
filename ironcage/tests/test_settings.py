3b429c4805afc0a5e3b0ecada4e46ff439a56ad3
Peter Inglesby <peter.inglesby@gmail.com>
2017-07-07 07:25:59 +0100

    Make flake8 happy

--- ironcage/tests/test_settings.py
+++ ironcage/tests/test_settings.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 class SettingsTests(TestCase):
     def test_prod_settings(self):
-        import ironcage.settings.prod
+        import ironcage.settings.prod  # noqa
 
     def test_staging_settings(self):
-        import ironcage.settings.staging
+        import ironcage.settings.staging  # noqa
