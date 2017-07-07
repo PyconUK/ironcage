6bfc449740a77e3adb199bece753524a7199ccdc
Peter Inglesby <peter.inglesby@gmail.com>
2017-07-07 07:27:05 +0100

    Add comments to tests

--- ironcage/tests/test_settings.py
+++ ironcage/tests/test_settings.py
@@ -3,7 +3,9 @@ from django.test import TestCase
 
 class SettingsTests(TestCase):
     def test_prod_settings(self):
+        # Test that prod settings can be imported
         import ironcage.settings.prod  # noqa
 
     def test_staging_settings(self):
+        # Test that staging settings can be imported
         import ironcage.settings.staging  # noqa
