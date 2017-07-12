834b3a1a39eff8ac20ee88dce0135854b07e8b55
Owen Campbell <owen.campbell@uk.python.org>
2017-07-12 10:27:15 +0100

    Add facility to withdraw grant applications

--- grants/urls.py
+++ grants/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^applications/new/$', views.new_application, name='new_application'),
     url(r'^applications/(?P<application_id>\w+)/$', views.application, name='application'),
     url(r'^applications/(?P<application_id>\w+)/edit/$', views.application_edit, name='application_edit'),
+    url(r'^applications/(?P<application_id>\w+)/delete/$', views.application_delete, name='application_delete'),
 ]
