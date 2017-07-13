a922d805072481a009546a6148a648256954afc0
Owen Campbell <owen.campbell@uk.python.org>
2017-07-13 09:08:15 +0100

    Fix PEP8 error

--- accounts/forms.py
+++ accounts/forms.py
@@ -88,7 +88,6 @@ class ProfileForm(forms.ModelForm):
             if value is not None and value not in choices:
                 widget.choices.insert(1, [value, value])
 
-
         # If the user has not bought a ticket, we do not offer UKPA membership.
         # The option is hidden in the templates, but we also disable the field
         # here to prevent that control being bypassed by a request sent
