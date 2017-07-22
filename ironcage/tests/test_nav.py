7f498ed48d5d15fe11b377191981005b2e43a052
Peter Inglesby <peter.inglesby@gmail.com>
2017-07-22 12:00:22 +0100

    Add navbar link to reports for staff users
    Fixes #96

--- ironcage/tests/test_nav.py
+++ ironcage/tests/test_nav.py
@@ -12,9 +12,21 @@ class NavTests(TestCase):
         self.assertContains(rsp, '<a href="/accounts/logout/">Sign out</a>', html=True)
         self.assertNotContains(rsp, 'Sign in')
         self.assertNotContains(rsp, 'Sign up')
+        self.assertNotContains(rsp, 'Reports')
 
     def test_user_not_authenticated(self):
         rsp = self.client.get('/tickets/orders/new/')
         self.assertContains(rsp, '<a href="/accounts/login/?next=/tickets/orders/new/">Sign in</a>', html=True)
         self.assertContains(rsp, '<a href="/accounts/register/?next=/tickets/orders/new/">Sign up</a>', html=True)
         self.assertNotContains(rsp, 'Sign out')
+        self.assertNotContains(rsp, 'Reports')
+
+    def test_user_is_staff(self):
+        user = accounts_factories.create_staff_user()
+        self.client.force_login(user)
+
+        rsp = self.client.get('/tickets/orders/new/')
+        self.assertContains(rsp, '<a href="/reports/">Reports</a>', html=True)
+        self.assertContains(rsp, '<a href="/accounts/logout/">Sign out</a>', html=True)
+        self.assertNotContains(rsp, 'Sign in')
+        self.assertNotContains(rsp, 'Sign up')
