f9a013c3d92aacf8c7f0361c6609e2c808bf2c37
Owen Campbell <owen.campbell@uk.python.org>
2017-07-27 16:54:22 +0100

    Add report for election candidates

--- reports/reports.py
+++ reports/reports.py
@@ -9,6 +9,7 @@ from cfp.models import Proposal
 from tickets.constants import DAYS
 from tickets.models import Order, Ticket
 from tickets.prices import cost_incl_vat
+from ukpa.models import Nomination
 
 
 @method_decorator(staff_member_required(login_url='login'), name='dispatch')
@@ -81,6 +82,23 @@ class UKPAReport(ReportView):
             'headings': ['Name', 'email'],
             'rows': rows,
         }
+
+
+class CandidateReport(ReportView):
+    title = 'Candidates for UKPA Trustee Election'
+
+    def get_context_data(self):
+        candidates = Nomination.objects.all()
+        rows = [
+            [candidate.nominee.name, candidate.statement]
+            for candidate in candidates]
+
+        return {
+            'title': self.title,
+            'headings': ['Name', 'Statement'],
+            'rows': rows,
+        }
+
 
 
 class TicketSalesReport(ReportView):
@@ -252,6 +270,7 @@ class SpeakersSeekingMentorReport(ReportView):
 reports = [
     AttendanceByDayReport,
     UKPAReport,
+    CandidateReport,
     TicketSalesReport,
     OrdersReport,
     UnpaidOrdersReport,
