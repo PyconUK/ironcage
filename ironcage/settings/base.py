67893ff0d594b4e7f828d6495963cf4df51d8f41
Peter Inglesby <peter.inglesby@gmail.com>
2017-10-22 19:12:10 +0100

    Set DEFAULT_FROM_EMAIL

--- ironcage/settings/base.py
+++ ironcage/settings/base.py
@@ -252,6 +252,7 @@ TICKET_DEADLINE_BYPASS_TOKEN = os.environ.get('TICKET_DEADLINE_BYPASS_TOKEN')
 
 # Email address to send mail from
 
+DEFAULT_FROM_EMAIL = 'PyCon UK 2017 <noreply@pyconuk.org>'
 SERVER_EMAIL = 'PyCon UK 2017 <noreply@pyconuk.org>'
 EMAIL_FROM_ADDR = f'PyCon UK 2017 <noreply@pyconuk.org>'
 EMAIL_REPLY_TO_ADDR = 'PyCon UK 2017 <pyconuk-committee@uk.python.org>'
