a3caff62eb016167282a4a53975a47fd79f0f725
Kirk Northrop <kirk@krn.me.uk>
2018-03-18 17:28:09 +0000

    Further add row tests

--- payments/tests/test_models.py
+++ payments/tests/test_models.py
@@ -25,12 +25,59 @@ class AddInvoiceRowTests(TestCase):
 
         self.assertEqual(self.invoice.total, invoice_row.total_inc_vat)
 
+    def test_add_invoice_row_to_invoice_zero_vat(self):
+        invoice_row = self.invoice.add_row(self.ticket, ZERO_RATE_VAT)
+
+        self.assertEqual(self.invoice.rows.count(), 1)
+
+        self.assertEqual(invoice_row.invoice_item, self.ticket)
+        self.assertEqual(invoice_row.total_ex_vat, self.ticket.cost_excl_vat())
+        self.assertEqual(invoice_row.vat_rate, ZERO_RATE_VAT)
+
+        self.assertEqual(self.invoice.total, invoice_row.total_inc_vat)
 
     def test_add_same_item_multiple_times_to_invoice_fails(self):
         self.invoice.add_row(self.ticket)
 
         with self.assertRaises(IntegrityError):
             self.invoice.add_row(self.ticket)
+
+    def test_add_two_items_to_invoice(self):
+        # arrange
+        bob = factories.create_user()
+        ticket_2 = ticket_factories.create_ticket(bob)
+
+        ticket_1_price = self.ticket.cost_incl_vat()
+        ticket_2_price = ticket_2.cost_incl_vat()
+
+        total_ticket_cost = ticket_1_price + ticket_2_price
+
+        # act
+        self.invoice.add_row(self.ticket)
+        self.invoice.add_row(ticket_2)
+
+        # assert
+        self.assertEqual(self.invoice.rows.count(), 2)
+        self.assertEqual(self.invoice.total, total_ticket_cost)
+
+    def test_add_two_items_to_invoice_different_vat_rates(self):
+        # arrange
+        bob = factories.create_user()
+        ticket_2 = ticket_factories.create_ticket(bob)
+
+        ticket_1_price = self.ticket.cost_incl_vat()
+        ticket_2_price = ticket_2.cost_excl_vat()
+
+        total_ticket_cost = ticket_1_price + ticket_2_price
+
+        # act
+        self.invoice.add_row(self.ticket)
+        self.invoice.add_row(ticket_2, ZERO_RATE_VAT)
+
+        # assert
+        self.assertEqual(self.invoice.rows.count(), 2)
+        self.assertEqual(self.invoice.total, total_ticket_cost)
+
 
 class InvoiceRowIncVatTests(TestCase):
     @classmethod
