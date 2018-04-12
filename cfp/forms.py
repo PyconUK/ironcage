ff4034b080cacee3ae80b973d8fb237404b13b70
Daniele Procida <daniele@vurt.org>
2018-04-12 12:35:51 +0100

    Added new CFP form fields

--- cfp/forms.py
+++ cfp/forms.py
@@ -14,24 +14,28 @@ class ProposalForm(forms.ModelForm):
             'subtitle',
             'copresenter_names',
             'description',
+            'outline',
             'description_private',
             'aimed_at_new_programmers',
             'aimed_at_teachers',
             'aimed_at_data_scientists',
             'would_like_mentor',
             'would_like_longer_slot',
+            'coc_conformity',
         ]
 
         labels = {
             'session_type': 'What are you proposing?',
             'copresenter_names': 'Are you presenting with anybody else?',
             'description': 'What is your session about?',
+            'outline': 'Can you give us an outline of your proposed session?',
             'description_private': 'Is there anything else we should know about your proposal?',
             'aimed_at_new_programmers': 'new programmers?',
             'aimed_at_teachers': 'teachers?',
             'aimed_at_data_scientists': 'data scientists?',
             'would_like_mentor': 'like a mentor',
             'would_like_longer_slot': 'like to be considered for a longer talk slot',
+            'coc_conformity': 'Does your proposal conform to our Code of Conduct?',
         }
 
         help_texts = {
@@ -39,7 +43,9 @@ class ProposalForm(forms.ModelForm):
             'subtitle': 'Limit: 120 characters.  Optional.',
             'copresenter_names': 'If you are presenting with anybody else, please list their names here.',
             'description': 'If your session is selected, this is the basis of what will be published in the programme.  Limit: 300 words.',
-            'description_private': 'Your answer here is for the benefit of the programme committee, and will not be published.  Limit: 300 words.',
+            'outline': 'An outline of your session is optional, but helps the programme committee. A proposal with an outline is more likely to be selected than one without. More detail, including timings, is better. The outline will not be published.',
+            'description_private': 'Your answer is for the benefit of the programme committee, and will not be published.  Limit: 300 words.',
+            'coc_conformity': 'I confirm that my proposed session conforms to the requirements of the <a href="https://2018.pyconuk.org/code-conduct/" target="_blank">Code of Conduct</a>',
         }
 
         widgets = {
@@ -47,6 +53,7 @@ class ProposalForm(forms.ModelForm):
             'subtitle': forms.TextInput(attrs={'placeholder': False}),
             'copresenter_names': forms.Textarea(attrs={'cols': 40, 'rows': 3, 'placeholder': False}),
             'description': forms.Textarea(attrs={'placeholder': False}),
+            'outline': forms.Textarea(attrs={'placeholder': False}),
             'description_private': forms.Textarea(attrs={'placeholder': False}),
         }
 
